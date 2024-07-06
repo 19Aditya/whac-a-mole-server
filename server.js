08f58dc98c6b76688b7c4924229a1bc76e369f9d
Harsh Sharma <awesomeaditya0@gmail.com>
2024-07-07 02:08:45 +0530

    to serve static files from public

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
 import cookieParser from "cookie-parser";
 import express, { json } from "express";
 import gameroute from "./routes/game.js";
 import loginroute from "./routes/login.js";
 import signuproute from "./routes/signup.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 const PORT = process.env.PORT ?? 3000;
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set("views", "./views");
 app.set("view engine", "ejs");
